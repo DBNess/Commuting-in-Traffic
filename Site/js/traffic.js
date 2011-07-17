607cc507ba4f69d393cdeb73dac882cc8fd60752
Vanessa Hurst <vrhurst@gmail.com>
2011-07-17 11:30:59 -0400

    add google analytics tracking

--- Site/js/traffic.js
+++ Site/js/traffic.js
@@ -90,7 +90,16 @@ HHH.LoadFact = function(questionID){
 	$(".inside").children("p").val(fact.anecdote);
 }
 
+//Google Analytics tracking
+var _gaq = _gaq || [];
+ _gaq.push(['_setAccount', 'UA-19096645-3']);
+ _gaq.push(['_trackPageview']);
 
+ (function() {
+   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ })();
 
 
 
